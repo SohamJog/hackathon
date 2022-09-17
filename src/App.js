b28c9fe6fad998c60a35d8a0fa23c88097fddbf4
Soham Jog <jogsoham2003@gmail.com>
2022-09-17 10:48:09 -0400

    basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component, useState } from "react";
+
+
+import "./App.css";
+import NavBar from './NavBar';
+
+class App extends Component {
+
+
+
+  render() {
+    
+    
+    return (
+      
+
+        <div className="App">
+
+
+          <div>Hello</div>
+          <NavBar />
+
+          
+
+          
+          
+
+          <div className="content">
+           
+          </div>
+        </div>
+
+
+
+    );
+  }
 }
 
 export default App;
+
+
+
+/*
+import React, { useEffect, useState } from 'react';
+import Voting from "./contracts/Voting.json";
+import getWeb3 from "./getWeb3";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import "./App.css";
+import NavBar from './NavBar';
+import Election from './Election';
+import Create from './Create';
+
+function App() {
+  const [account, setAccount] = useState(); // state variable to set account.
+  
+  useEffect(() => {
+    async function load() {
+      const web3 = await getWeb3();
+
+      const accounts = await web3.eth.requestAccounts();
+      
+      setAccount(accounts[0]);
+    }
+    
+    load();
+   }, []);
+  
+   return (
+     <div>
+       Your account is: {account}
+     </div>
+   );
+}
+
+export default App;
+
+
+*/
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const NavBar = () => {
+
+
+    return ( 
+        <nav >
+
+            <h1>Decentralized Polls</h1>
+
+           
+
+            <span><a href="/">Home</a></span>
+
+
+        </nav>
+     );
+}
+ 
+export default NavBar;
